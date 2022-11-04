27b3cbdb0fac334b64d7dfaeade8df7c892bce70
it-dude-ru <it-dude@ya.ru>
2022-11-04 23:19:29 +0300

    hw 5.9

--- src/routes/Game/game.jsx
+++ src/routes/Game/game.jsx
@@ -4,16 +4,46 @@ import PokemonCard from '../../components/PokemonCard/pokemon-card';
 
 import database from '../../services/firebase';
 
-import './style.module.css';
+import s from './style.module.css';
+
+const DATA = {
+	"abilities": [
+		"keen-eye",
+		"tangled-feet",
+		"big-pecks"
+	],
+	"stats": {
+		"hp": 63,
+		"attack": 60,
+		"defense": 55,
+		"special-attack": 50,
+		"special-defense": 50,
+		"speed": 71
+	},
+	"type": "flying",
+	"img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+	"name": "pidgeotto",
+	"base_experience": 122,
+	"height": 11,
+	"id": 17,
+	"values": {
+		"top": "A",
+		"right": 2,
+		"bottom": 7,
+		"left": 5
+	}
+}
 
 const GamePage = () => {
 	const [pokemons, setPokemons] = useState({});
 
-	useEffect(() => {
+	const getPokemons = () => {
 		database.ref('pokemons').once('value', (snapshot) => {
 			setPokemons(snapshot.val());
-		})
-	}, [])
+		});
+	}
+
+	useEffect(getPokemons(), []);
 
 	const handleChangeActive = (id) => {
 		setPokemons(prevState => {
@@ -23,19 +53,26 @@ const GamePage = () => {
 					pokemon.active = true;
 				};
 				acc[item[0]] = pokemon;
+				database.ref('pokemons/' + item[0]).set(pokemon);
 				return acc;
 			}, {});
 		});
-
-		// setPokemons(prevState => {
-		// 	return Array.from(prevState, (item) => {
-		// 		if (item.id === id) item.active = true;
-		// 		return item;
-		// 	});
-		// });
 	}
+
+	const handleAddPokemon = () => {
+		const data = DATA;
+		const newKey = database.ref().child('pokemons').push().key;
+		database.ref('pokemons/' + newKey).set(data).then(() => getPokemons());
+	}
+
 	return (
-		<div className="flex">
+		<>
+		<div className={s.buttonWrap}>
+				<button onClick={handleAddPokemon}>
+					Add New Pokemon
+				</button>
+		</div>
+		<div className={s.flex}>
 			{
 				Object.entries(pokemons).map(([key, { name, img, id, type, values, active }]) => (
 					<PokemonCard
@@ -50,7 +87,8 @@ const GamePage = () => {
 					/>))
 			}
 		</div>
-	)
+		</>
+	);
 }
 
 export default GamePage;
