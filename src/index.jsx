2753998cf946148328f138da2e3332c8d26c9782
it-dude-ru <it-dude@ya.ru>
2023-07-26 17:35:16 +0300

    Test counter removed, cmpnts from hw8 added

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import store from './store/store';
+import store from './store/store';
 
 import App from './App';
 import './index.css';
-
-import { createStore, bindActionCreators } from 'redux';
-import rootReducer, * as actions from './store/counter';
-
-const store = new createStore(rootReducer);
-console.log('###: store: ', store.getState());
-store.subscribe(() => console.log('store subscribe ', store.getState()));
-const {plusAction, minusAction} = bindActionCreators(actions, store.dispatch);
-
-plusAction(5);
-plusAction(3);
-plusAction(10);
-minusAction(25);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
